3528c62d9aa7ff0788e561fee6dff11091bbd261
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-21 12:58:15 +0300

    Add file selector plugin support and update dependencies for image picking functionality

--- lib/features/add_product/presentation/view/widgets/image_file.dart
+++ lib/features/add_product/presentation/view/widgets/image_file.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageFile extends StatelessWidget {
   const ImageFile({super.key});
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () async {
+        final ImagePicker picker = ImagePicker();
+        final XFile? image = await picker.pickImage(
+          source: ImageSource.gallery,
+        );
+      },
       child: Container(
         width: double.infinity,
         decoration: BoxDecoration(
