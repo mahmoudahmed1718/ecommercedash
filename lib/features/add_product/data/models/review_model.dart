4e10b94a7c1e70bab096ab87bd1c930198b765f8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-24 18:55:49 +0300

    Implement ReviewModel class with factory methods for entity conversion and JSON serialization

--- lib/features/add_product/data/models/review_model.dart
+++ lib/features/add_product/data/models/review_model.dart
@@ -1,3 +1,38 @@
-// import 'package:ecommercedash/features/add_product/domain/entities/review_entity.dart';
+import 'package:ecommercedash/features/add_product/domain/entities/review_entity.dart';
 
-// class ReviewModel extends ReviewEntity {}
+class ReviewModel extends ReviewEntity {
+  ReviewModel({
+    required super.name,
+    required super.description,
+    required super.rating,
+    required super.image,
+    required super.data,
+  });
+  factory ReviewModel.fromEntity(ReviewEntity entity) {
+    return ReviewModel(
+      name: entity.name,
+      description: entity.description,
+      rating: entity.rating,
+      image: entity.image,
+      data: entity.data,
+    );
+  }
+  factory ReviewModel.fromJson(Map<String, dynamic> json) {
+    return ReviewModel(
+      name: json['name'],
+      description: json['description'],
+      rating: json['rating'],
+      image: json['image'],
+      data: json['data'],
+    );
+  }
+  toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'rating': rating,
+      'image': image,
+      'data': data,
+    };
+  }
+}
