edfeae24debcb81ded5158684c1ebccaff816e5d
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-24 12:01:43 +0300

    Add dartz dependency and implement ProductRepo and ProductRepoImpl classes for product management

---
+++ lib/core/repo/product_repo.dart
@@ -0,0 +1,5 @@
+import 'package:ecommercedash/features/add_product/domain/entities/add_product_input_entity.dart';
+
+abstract class ProductRepo {
+  Future<void> addProduct(AddProductInputEntity addProductInputEntity);
+}
---
+++ lib/core/repo/product_repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:ecommercedash/core/repo/product_repo.dart';
+import 'package:ecommercedash/features/add_product/domain/entities/add_product_input_entity.dart';
+
+class ProductRepoImpl implements ProductRepo {
+  @override
+  Future<void> addProduct(AddProductInputEntity addProductInputEntity) {
+    // TODO: implement addProduct
+    throw UnimplementedError();
+  }
+}
