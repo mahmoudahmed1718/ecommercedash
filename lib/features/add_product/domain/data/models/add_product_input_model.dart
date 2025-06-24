3f7ff2604cab5c07097a31604043a58dae0ee3ea
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-24 14:34:03 +0300

    Add AddProductInputModel class for managing product input data

---
+++ lib/features/add_product/domain/data/models/add_product_input_model.dart
@@ -0,0 +1,36 @@
+import 'package:ecommercedash/features/add_product/domain/entities/add_product_input_entity.dart';
+
+class AddProductInputModel extends AddProductInputEntity {
+  AddProductInputModel({
+    required super.name,
+    required super.description,
+    required super.price,
+    required super.code,
+    required super.imageFile,
+    required super.isFeatured,
+    super.imageUrl,
+  });
+
+  factory AddProductInputModel.fromEntity(AddProductInputEntity entity) {
+    return AddProductInputModel(
+      name: entity.name,
+      description: entity.description,
+      price: entity.price,
+      code: entity.code,
+      imageFile: entity.imageFile,
+      isFeatured: entity.isFeatured,
+      imageUrl: entity.imageUrl,
+    );
+  }
+  toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'price': price,
+      'code': code,
+      'imageFile': imageFile,
+      'imageUrl': imageUrl,
+      'isFeatured': isFeatured,
+    };
+  }
+}
