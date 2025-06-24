6db5457a7b3c40859b61c284cd731de0f2420b32
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-06-24 18:54:34 +0300

    Add ReviewEntity class with fields for name, description, rating, image, and data

---
+++ lib/features/add_product/data/models/review_model.dart
@@ -0,0 +1,3 @@
+// import 'package:ecommercedash/features/add_product/domain/entities/review_entity.dart';
+
+// class ReviewModel extends ReviewEntity {}
---
+++ lib/features/add_product/domain/entities/review_entity.dart
@@ -0,0 +1,15 @@
+class ReviewEntity {
+  final String name;
+  final String description;
+  final num rating;
+  final String image;
+  final String data;
+
+  ReviewEntity({
+    required this.name,
+    required this.description,
+    required this.rating,
+    required this.image,
+    required this.data,
+  });
+}
